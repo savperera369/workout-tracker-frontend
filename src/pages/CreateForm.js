4e513d24f3ef89f52ea4651c1587c76642b53933
savperera369 <saviru2002@outlook.com>
2023-06-19 16:43:25 -0400

    updates to form

--- src/pages/CreateForm.js
+++ src/pages/CreateForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../index.css';
+import { useState } from "react";
 import { Button, Container, Typography } from "@mui/material";
 
 // controlled inputs are a way of setting up input fields in forms
@@ -12,6 +13,15 @@ import { Button, Container, Typography } from "@mui/material";
 
 function CreateForm() {
     // label - for first field
+    // following state variables track userInput so we can do something with it
+    // associate value of following state variables with values of input fields
+    // this makes whatever the value of the variables show up in the input field
+    // onchange event - when we change text in input field, it calls setFunctions to update value of state variables
+    // event.target.value -> event is typing into field, target is input element, value is whatever we typed
+    const [name, setName] = useState("");
+    const [duration, setDuration] = useState(-1);
+    const [date, setDate] = useState("");
+
     return (
         <Container>
             <Typography variant="h4">Add a New Workout</Typography>
@@ -21,13 +31,17 @@ function CreateForm() {
                 type="text"
                 required
                 className="create"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
                 />
                 <br />
-                <label className="create">Workout Duration: </label>
+                <label className="create">Workout Duration in Minutes: </label>
                 <input 
                 type="text"
                 required
                 className="create"
+                value={duration}
+                onChange={(event) => setDuration(event.target.value)}
                 />
                 <br />
                 <label className="create">Workout Date: </label>
@@ -35,6 +49,8 @@ function CreateForm() {
                 type="text"
                 required
                 className="create"
+                value={date}
+                onChange={(event) => setDate(event.target.value)}
                 />
                 <br />
                 <Button variant="contained">Add Workout</Button>
