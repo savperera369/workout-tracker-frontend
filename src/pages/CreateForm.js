e150dfff40c491cdae8771898675e7cb4533d838
savperera369 <saviru2002@outlook.com>
2023-06-19 23:12:10 -0400

    adding a workout succesfully pushes to database

--- src/pages/CreateForm.js
+++ src/pages/CreateForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../index.css';
-import { useState } from "react";
-import { Button, Container, Typography } from "@mui/material";
+import { useState} from "react";
+import { Container, Typography } from "@mui/material";
 
 // controlled inputs are a way of setting up input fields in forms
 // so that we can track their values
@@ -18,14 +18,40 @@ function CreateForm() {
     // this makes whatever the value of the variables show up in the input field
     // onchange event - when we change text in input field, it calls setFunctions to update value of state variables
     // event.target.value -> event is typing into field, target is input element, value is whatever we typed
+    // when a button is pressed inside a form it fires a submit event (onSubmit) on the form itself
+    // can listen for the submit event and react to it
+
     const [name, setName] = useState("");
-    const [duration, setDuration] = useState(-1);
+    const [duration, setDuration] = useState(0);
     const [date, setDate] = useState("");
+
+    // first thing to do on submit - stop default action of form being submitted
+    // which is page refreshing
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // want to create a newworkout object
+        // this will ultimately be saved to DB
+        // create an object consisting of the current values of the state variables
+        const newWorkout = {name, duration, date};
+        // send post request to server to save newWorkout to DB
+        // fetch is asynchronous, returns a promise so we tack on a then
+        fetch("/api/workouts", {
+            method: "POST",
+            // tells server we are sending json data
+            headers: {"Content-Type": "application/json"},
+            // actual data we are sending, in JSON form
+            body: JSON.stringify(newWorkout)
+        }).then(() => {
+            setName("");
+            setDuration(0);
+            setDate("");
+        })
+    };
 
     return (
         <Container>
             <Typography variant="h4">Add a New Workout</Typography>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="create">Workout Name: </label>
                 <input 
                 type="text"
@@ -53,7 +79,7 @@ function CreateForm() {
                 onChange={(event) => setDate(event.target.value)}
                 />
                 <br />
-                <Button variant="contained">Add Workout</Button>
+                <button>Add Workout</button>
             </form>
         </Container>
     );
